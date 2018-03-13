338eaaae441080c7bbaee9721a4028513a9886df
joe <josephtye1@gmail.com>
2018-03-12 23:07:53 -0500

    edit map

--- public/plugins/maps/map1.js
+++ public/plugins/maps/map1.js
@@ -1,5 +1,12 @@
-/*        var mymap = L.map('mapid').setView([33.2,-97.1],14);
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position){
+    });
 
+}else {
+    alert("Geolocation API is not supported in your browser. :(");
+}
+
+var mymap = L.map('mapid').setView([31.2, -91.1],14);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 20,
@@ -7,7 +14,4 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoicmVzcXVlbWUiLCJhIjoiY2pkZG91aDNuMDRsMDJ3bmpnOGJpNGNlNyJ9.hwp2QeD6easWjlAlaEIJ2w'
 }).addTo(mymap);
 
-<!--  <script src="/plugins/maps/map1.js"></script> -->
-
-*/
-
+mymap.locate({setView: true});
