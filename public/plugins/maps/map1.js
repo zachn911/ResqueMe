b989f06df626c09fdc25613e28903e4f608e8443
joe <josephtye1@gmail.com>
2018-03-14 02:48:27 -0500

    edit map

--- public/plugins/maps/map1.js
+++ public/plugins/maps/map1.js
@@ -7,10 +7,10 @@ if (navigator.geolocation) {
 }
 
 var mymap = L.map('mapid').setView([31.2, -91.1],14);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.streets',
+    id: 'mapbox.satellite-streets',
     accessToken: 'pk.eyJ1IjoicmVzcXVlbWUiLCJhIjoiY2pkZG91aDNuMDRsMDJ3bmpnOGJpNGNlNyJ9.hwp2QeD6easWjlAlaEIJ2w'
 }).addTo(mymap);
 
