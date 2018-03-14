e45d424bcb266bc1a77daa0d105231b4e401d9ee
joe <josephtye1@gmail.com>
2018-03-14 04:51:14 -0500

    edit map

--- public/plugins/maps/map1.js
+++ public/plugins/maps/map1.js
@@ -9,7 +9,7 @@ if (navigator.geolocation) {
 var mymap = L.map('mapid').setView([31.2, -91.1],14);
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 20,
     id: 'mapbox.satellite-streets',
     accessToken: 'pk.eyJ1IjoicmVzcXVlbWUiLCJhIjoiY2pkZG91aDNuMDRsMDJ3bmpnOGJpNGNlNyJ9.hwp2QeD6easWjlAlaEIJ2w'
 }).addTo(mymap);
@@ -17,14 +17,13 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/
 mymap.locate({
     watch: false,
     setView: true,
-    maxZoom: 16,
     enableHighAccuracy: true
 })
 
 function onLocationFound(e) {
     var radius = e.accuracy/2;
-
-    L.circle(e.latlng, 16).addTo(mymap);
+    mymap.setZoom(16);
+    L.circle(e.latlng, 30).addTo(mymap);
 
     L.circle(e.latlng, 1).addTo(mymap);
 }
@@ -33,9 +32,12 @@ mymap.on('locationfound', onLocationFound);
 
 var la = 33.215;
 var lo = -97.1331;
-L.marker([la, lo]).addTo(mymap);
+marker = L.marker([la, lo]).addTo(mymap);
+marker.bindPopup('This is at: ' + la + ', ' + lo);
 
+/*
 mymap.on('click', function(e) {
     L.marker([e.latlng.lat, e.latlng.lng]).addTo(mymap);
     marker.bindPopup('This is at: ' + lat + ', ' + lng).openPopup();
 } );
+*/
--- public/plugins/maps/rMap.js
+++ public/plugins/maps/rMap.js
@@ -9,7 +9,7 @@ if (navigator.geolocation) {
 var mymap = L.map('mapid').setView([31.2, -91.1],14);
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 20,
     id: 'mapbox.satellite-streets',
     accessToken: 'pk.eyJ1IjoicmVzcXVlbWUiLCJhIjoiY2pkZG91aDNuMDRsMDJ3bmpnOGJpNGNlNyJ9.hwp2QeD6easWjlAlaEIJ2w'
 }).addTo(mymap);
@@ -17,13 +17,12 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/
 mymap.locate({
     watch: false,
     setView: true,
-    maxZoom: 18,
     enableHighAccuracy: true
 })
 
 function onLocationFound(e) {
     var radius = e.accuracy/2;
-
+    mymap.setZoom(16);
     L.circle(e.latlng, 4).addTo(mymap);
 
     L.circle(e.latlng, 1).addTo(mymap);
