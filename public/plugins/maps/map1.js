06e0aec0736ecc793b5dabf47198c36b0bb81491
joe <josephtye1@gmail.com>
2018-03-15 09:46:47 -0500

    edit map

--- public/plugins/maps/map1.js
+++ public/plugins/maps/map1.js
@@ -18,7 +18,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/
 }).addTo(mymap);
 
 mymap.locate({
-    watch: true,
+    watch: false,
     setView: true,
     enableHighAccuracy: true
 })
