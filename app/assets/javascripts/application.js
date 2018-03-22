906d1686f007471077360f2d648f4cf82120e8ad
zachn911 <zach@ongoingtech.com>
2018-03-21 23:27:41 -0500

    changing routes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,14 @@
 //= require custom/tree-menu
 //= require main
 //= require leaflet
+alert ('This site is still in production release date will be mid May 2018');
+//var text;
+//var favDrink = prompt("Enter password:");
+//switch(favDrink) {
+//    case "aaaaaaaaaaaaa":
+//        text = "Excellent choice!";
+//        break;
+//    default:
+//        window.location.replace("http://www.misterfixit.com/lastpage.html");
+//        break;
+//}
