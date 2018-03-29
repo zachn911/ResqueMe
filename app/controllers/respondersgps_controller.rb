4007292afbf425363309c53f3b2d493b45482024
zachn911 <zach@ongoingtech.com>
2018-03-28 23:58:04 -0500

    connected db to view

--- app/controllers/respondersgps_controller.rb
+++ app/controllers/respondersgps_controller.rb
@@ -3,8 +3,9 @@ class RespondersgpsController < ApplicationController
   def rgps
     render template: "respondersgps/#{params[:rgps]}"
   end
-  def index
-  end
 
+  def index
+      @responder = Responder.all
+  end
 
 end
