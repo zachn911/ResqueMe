1a50f053a00861429ff89d7e8523649e8950743e
joe <josephtye1@gmail.com>
2018-04-03 05:01:06 -0500

    remove duplicate code

--- public/plugins/maps/rMap.js
+++ public/plugins/maps/rMap.js
@@ -13,12 +13,10 @@ mymap.on('locationfound', onLocationFound);
 
    // var myEle =  document.getElementById("res-info1");
    var i;
-    var c = 1;
-   for (i = 0; i < 10000; i++){
-        var lt = $('#res-info'+c).data('lat');
-        var lo = $('#res-info'+c).data('long');
+   for (i = 1; i < 10000; i++){
+        var lt = $('#res-info'+i).data('lat');
+        var lo = $('#res-info'+i).data('long');
         L.marker([lt, lo]).addTo(mymap).bindPopup("popup");
-        c++;}
+        }
 //alert("Testing!");
-//alert('the index is: ' + rando );
 
