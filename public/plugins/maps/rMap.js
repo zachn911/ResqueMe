38bca7a524918e15644ec8714b30a93c6008eb6e
joe <josephtye1@gmail.com>
2018-04-03 03:24:40 -0500

    adding icons from database data

--- public/plugins/maps/rMap.js
+++ public/plugins/maps/rMap.js
@@ -12,3 +12,14 @@ function onLocationFound(e) {
 
 mymap.on('locationfound', onLocationFound);
 
+    var rando= $('#res-info').data('indx');
+    var lt = $('#res-info').data('lat');
+    var lo = $('#res-info').data('long');
+    L.marker([lt, lo]).addTo(mymap).bindPopup("Ambulance # 26; Loc: 33.16, -97.2");
+
+
+
+//var lg = '<%= respondern.long %>';
+//alert("Testing!");
+//alert('the index is: ' + rando );
+
