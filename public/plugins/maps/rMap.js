017daa3fcd695fc389ce869ad2b8490b93c08286
joe <josephtye1@gmail.com>
2018-04-03 04:58:26 -0500

    add location found to responder map

--- public/plugins/maps/rMap.js
+++ public/plugins/maps/rMap.js
@@ -1,4 +1,16 @@
 
+mymap.locate({
+    watch: false,
+    setView: false,
+    enableHighAccuracy: true
+})
+
+function onLocationFound(e) {
+    mymap.panTo(e.latlng);
+    L.marker(e.latlng, {icon: circleIcon}).bindPopup('Current location: ' + e.latlng.lat.toPrecision(6) + ', ' + e.latlng.lng.toPrecision(6)).addTo(mymap);
+}
+mymap.on('locationfound', onLocationFound);
+
    // var myEle =  document.getElementById("res-info1");
    var i;
     var c = 1;
