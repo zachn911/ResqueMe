ba84633bcf7333526768ce3724a0a808f0f3a2b8
zachn911 <zach@ongoingtech.com>
2018-04-11 01:24:43 -0500

    connected db to view

--- app/models/vehiclenode.rb
+++ app/models/vehiclenode.rb
@@ -1,4 +1,10 @@
 class Vehiclenode < ApplicationRecord
-  belongs_to :vehicle
+  belongs_to :vehicle, :class_name => Vehicle, :foreign_key => "vehicle_id"
+  delegate :vehicle_id, :to => :vehicle
+  delegate :v_type, :to => :vehicle
+  delegate :make, :to => :vehicle
+  delegate :model, :to => :vehicle
+  delegate :color, :to => :vehicle
+  delegate :equipment, :to => :vehicle
   belongs_to :sensorauth
 end
