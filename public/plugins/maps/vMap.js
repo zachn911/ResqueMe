ce31064861b6936ab5f4cb923dca8649915268fe
joe <josephtye1@gmail.com>
2018-04-11 10:15:33 -0500

    center map icons

--- public/plugins/maps/vMap.js
+++ public/plugins/maps/vMap.js
@@ -1,31 +1,30 @@
-/*
-mymap.locate({
-    watch: false,
-    setView: false,
-    enableHighAccuracy: true
-})
 
-function onLocationFound(e) {
-    mymap.panTo(e.latlng);
-    L.marker(e.latlng, {icon: circleIcon}).bindPopup("Current location.").addTo(mymap);
-}
-mymap.on('locationfound', onLocationFound);
-*/
 var i;
-for (i = 1; i < 10000; i++){
+for (i = 1; i < 1000; i++){
     var lt = $('#veh-info'+i).data('lat');
     var lo = $('#veh-info'+i).data('long');
     var time = $('#veh-info'+i).data('time');
     var type = $('#veh-info'+i).data('type');
 
-    if (type == 'ambulance') {
-        L.marker([lt, lo], {icon: ambulanceIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
-    } else if (type == 'truck') {
-        L.marker([lt, lo], {icon: truckIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
-    } else if (type == 'boat') {
-        L.marker([lt, lo], {icon: boatIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
+    if (lt === undefined) {
+        i = 1000;
     } else {
-        L.marker([lt, lo], {icon: carIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
+        if (type == 'ambulance') {
+            L.marker([lt, lo], {icon: ambulanceIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
+        } else if (type == 'truck') {
+            L.marker([lt, lo], {icon: truckIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
+        } else if (type == 'boat') {
+            L.marker([lt, lo], {icon: boatIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
+        } else if (type == 'firefighter') {
+            L.marker([lt, lo], {icon: firefighterIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
+        } else if (type == 'police') {
+            L.marker([lt, lo], {icon: policeIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
+        } else if (type == 'helo') {
+            L.marker([lt, lo], {icon: heloIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
+        } else {
+            L.marker([lt, lo], {icon: carIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
+        }
     }
 
 }
+
