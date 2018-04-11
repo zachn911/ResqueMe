a03c8ebaac06c0839244320c45dd9ef22ce83cfe
joe <josephtye1@gmail.com>
2018-04-10 23:11:53 -0500

    edit vehicle map

--- public/plugins/maps/vMap.js
+++ public/plugins/maps/vMap.js
@@ -11,11 +11,11 @@ function onLocationFound(e) {
 }
 mymap.on('locationfound', onLocationFound);
 
-/*var i;  //uncomment this once the controller issue is fixed
+var i;
 for (i = 1; i < 10000; i++){
     var lt = $('#veh-info'+i).data('lat');
     var lo = $('#veh-info'+i).data('long');
     var time = $('#veh-info'+i).data('time');
 
     L.marker([lt, lo], {icon: ambulanceIcon}, {riseOnHover: true}).addTo(mymap).bindPopup('Make<br>Model<br>Loc: ' + lt + ', ' + lo + '<br>Last Update: ' + time);
-}*/
+}
